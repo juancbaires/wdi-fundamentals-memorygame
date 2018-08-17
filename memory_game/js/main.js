33e7f60c921bb3c9d725d150c2a662438b8ecd3c
Juan Carlos Baires <rebecamartinez@Juans-MacBook-Pro-2.local>
2018-08-17 18:53:32 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,18 @@
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("user flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] ===cardsInPlay[1]){
-	alert("you found a match!");
-}else{
-	alert("sorry, try again")
+var checkForMath = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
 }
+}
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMath();{
+
+	}
+}
+flipCard(0);
+flipCard(2);
